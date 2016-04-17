c6c3b14fc604fd5aff7344ac34b307b81945b8a1
rexim <reximkut@gmail.com>
2016-04-17 23:26:59 +0600

    Implement adding point

--- src/main.ml
+++ src/main.ml
@@ -1,3 +1,5 @@
+open Graphics
+
 let test_ps = [(1, 1);
                (2, 5);
                (3, 3);
@@ -5,4 +7,20 @@ let test_ps = [(1, 1);
                (3, 2);
                (2, 2)]
 
-let _ = print_string "Hello, World!\n"
+let ps = ref []
+
+let render_state () =
+  clear_graph ();
+  List.iter (fun (x, y) -> draw_circle x y 20) !ps
+
+let prev_button_state = ref false
+
+let rec event_loop () =
+  let current_button_state = button_down () in
+  (match (current_button_state, !prev_button_state) with
+   | (true, false) -> ps := mouse_pos () :: !ps;
+                      render_state ()
+   | (false, true) -> ()
+   | _ -> ());
+  prev_button_state := current_button_state;
+  event_loop ()
