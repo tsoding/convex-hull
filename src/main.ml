d38da33c25e614e463b3224263ab7aff980c8cac
rexim <reximkut@gmail.com>
2016-04-17 23:54:40 +0600

    Draw convex hull for the points

--- src/main.ml
+++ src/main.ml
@@ -1,26 +1,32 @@
 open Graphics
-
-let test_ps = [(1, 1);
-               (2, 5);
-               (3, 3);
-               (5, 3);
-               (3, 2);
-               (2, 2)]
+open Convexhull
 
 let ps = ref []
 
 let render_state () =
   clear_graph ();
-  List.iter (fun (x, y) -> draw_circle x y 20) !ps
+  let ch = convex_hull !ps in
+  List.iter (fun (x, y) -> draw_circle x y 20) !ps;
+  draw_poly @@ Array.of_list ch
 
 let prev_button_state = ref false
+
+let is_mouse_pos_ok (pos_x, pos_y) =
+  0 <= pos_x && pos_x < size_x () && 0 <= pos_y && pos_y < size_y ()
 
 let rec event_loop () =
   let current_button_state = button_down () in
   (match (current_button_state, !prev_button_state) with
-   | (true, false) -> ps := mouse_pos () :: !ps;
-                      render_state ()
+   | (true, false) -> let mp = mouse_pos () in
+                      if is_mouse_pos_ok mp
+                      then ps := mp :: !ps; render_state ()
    | (false, true) -> ()
    | _ -> ());
   prev_button_state := current_button_state;
   event_loop ()
+
+let _ =
+  open_graph "";
+  resize_window 800 600;;
+  event_loop ();
+  close_graph ()
