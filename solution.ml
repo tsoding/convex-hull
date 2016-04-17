140ae7d3c3e889c8eb0f59424139a88238075511
rexim <reximkut@gmail.com>
2016-04-17 17:47:48 +0600

    Remove trailing whitespaces

--- solution.ml
+++ solution.ml
@@ -9,14 +9,14 @@ let test_ps = [(1, 1);
                (3, 2);
                (2, 2)]
 
-let find_start_point ps = 
+let find_start_point ps =
   List.fold_left min (max_int, max_int) ps
-  
-let angle_sort (sx, sy) ps = 
+
+let angle_sort (sx, sy) ps =
   let vs = List.map (fun (px, py) -> (px - sx, py - sy)) ps in
-  let angles = List.map (fun (vx, vy) -> atan2 (float_of_int vy) (float_of_int vx)) vs 
-  in angles 
-     |> List.combine ps 
+  let angles = List.map (fun (vx, vy) -> atan2 (float_of_int vy) (float_of_int vx)) vs
+  in angles
+     |> List.combine ps
      |> List.sort (fun (_, a1) (_, a2) -> compare a1 a2)
      |> List.map fst
 
@@ -27,20 +27,20 @@ let check_clockwise (x1, y1) (x2, y2) (x3, y3) =
 
 let rec graham_scan sorted_ps =
   match sorted_ps with
-  | p1 :: p2 :: p3 :: rest_sorted_ps -> 
+  | p1 :: p2 :: p3 :: rest_sorted_ps ->
      if check_clockwise p1 p2 p3
      then p1 :: graham_scan (p2 :: p3 :: rest_sorted_ps)
      else graham_scan (p1 :: p3 :: rest_sorted_ps)
   | rest_sorted_ps -> rest_sorted_ps
 
-let convex_hull ps = 
+let convex_hull ps =
   if List.length ps > 3
   then let start_point = find_start_point ps in
        let sorted_ps = angle_sort start_point ps in
        graham_scan sorted_ps
   else ps
 
-let distance (x1, y1) (x2, y2) = 
+let distance (x1, y1) (x2, y2) =
   let dx = float_of_int (x2 - x1) in
   let dy = float_of_int (y2 - y1) in
   sqrt (dx *. dx +. dy *. dy)
