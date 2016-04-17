7d48c78de197419a5fdfc5164ab5def31fde0d88
rexim <reximkut@gmail.com>
2016-04-18 00:48:36 +0600

    Fix Graham Scan algorithm

--- src/convexhull.ml
+++ src/convexhull.ml
@@ -12,22 +12,26 @@ let angle_sort (sx, sy) ps =
      |> List.sort (fun (_, a1) (_, a2) -> compare a1 a2)
      |> List.map fst
 
-let check_clockwise (x1, y1) (x2, y2) (x3, y3) =
+let ccw (x1, y1) (x2, y2) (x3, y3) =
   let (v1x, v1y) = (x2 - x1, y2 - y1) in
   let (v2x, v2y) = (x3 - x2, y3 - y2) in
-  v1x * v2y - v1y * v2x < 0
+  v1x * v2y - v1y * v2x
+
+let rec drop_until_ccw p1 convex_hull =
+  match convex_hull with
+  | p2 :: p3 :: rest_convex_hull when ccw p1 p2 p3 <= 0 ->
+     drop_until_ccw p1 (p3 :: rest_convex_hull)
+  | rest_convex_hull -> rest_convex_hull
 
 let rec graham_scan sorted_ps =
   match sorted_ps with
-  | p1 :: p2 :: p3 :: rest_sorted_ps ->
-     if check_clockwise p1 p2 p3
-     then p1 :: graham_scan (p2 :: p3 :: rest_sorted_ps)
-     else graham_scan (p1 :: p3 :: rest_sorted_ps)
-  | rest_sorted_ps -> rest_sorted_ps
+  | p1 :: rest_sorted_ps -> let rest_convex_hull = drop_until_ccw p1 @@ graham_scan rest_sorted_ps in
+                            p1 :: rest_convex_hull
+  | [] -> []
 
 let convex_hull ps =
   if List.length ps > 3
   then let start_point = find_start_point ps in
-       let sorted_ps = angle_sort start_point ps in
+       let sorted_ps = start_point :: (angle_sort start_point @@ List.filter (fun p -> p != start_point) ps) in
        graham_scan sorted_ps
   else ps
--- src/main.ml
+++ src/main.ml
@@ -6,7 +6,7 @@ let ps = ref []
 let render_state () =
   clear_graph ();
   let ch = convex_hull !ps in
-  List.iter (fun (x, y) -> draw_circle x y 20) !ps;
+  List.iter (fun (x, y) -> draw_circle x y 10) !ps;
   draw_poly @@ Array.of_list ch
 
 let prev_button_state = ref false
@@ -14,7 +14,7 @@ let prev_button_state = ref false
 let is_mouse_pos_ok (pos_x, pos_y) =
   0 <= pos_x && pos_x < size_x () && 0 <= pos_y && pos_y < size_y ()
 
-let rec event_loop () =
+let rec handle_mouse () =
   let current_button_state = button_down () in
   (match (current_button_state, !prev_button_state) with
    | (true, false) -> let mp = mouse_pos () in
@@ -22,11 +22,18 @@ let rec event_loop () =
                       then ps := mp :: !ps; render_state ()
    | (false, true) -> ()
    | _ -> ());
-  prev_button_state := current_button_state;
+  prev_button_state := current_button_state
+
+let rec handle_keyboard () =
+  if key_pressed () then (if read_key () == ' ' then ps := []; render_state ())
+
+let rec event_loop () =
+  handle_mouse ();
+  handle_keyboard ();
   event_loop ()
 
 let _ =
   open_graph "";
-  resize_window 800 600;;
+  resize_window 800 400;;
   event_loop ();
   close_graph ()
